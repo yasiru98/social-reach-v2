43b968c89e0471545af220b76f9b368aa83ad9c1
yasiru98 <yasiru043@gmail.com>
2021-10-07 16:46:06 +0530

    grid system

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,14 +4,14 @@ import Container from '@material-ui/core/Container';
 
 import Section1 from './../section-1';
 import Section2 from './../section-2';
-import Section3 from './../section-3';
+import CardGrid from './../section-3';
 function App() {
   return (
 
     <Container maxWidth="xl" className="App">
       <Section1 />
       <Section2 />
-      <Section3 />
+      <CardGrid />
     </Container>
   );
 }
--- src/components/section-3.js
+++ src/components/section-3.js
@@ -1,24 +1,59 @@
-import Typography from '@material-ui/core/Typography';
-import logo from './../logo.svg';
-import { Button, Paper,Box } from '@material-ui/core';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  CardHeader
+} from "@material-ui/core/";
 
-
-  
-  function Section2(props) {
-    return (
-        <Box sx={{ width: '100%', height: '100vh' }}>
-        <img src={logo} className="App-logo" alt="logo" />
-        <Typography variant="h4" component="h1" gutterBottom>
-          Section 3
-        </Typography>
-        <Button variant="contained" color="primary">
-          Primary Button
-        </Button>
-        <Button variant="contained" color="secondary">
-          Secondary Button
-        </Button>
-      </Box>
-    );
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: theme.spacing(2)
   }
-  
-  export default Section2;
+}));
+
+function CardGrid() {
+  const classes = useStyles();
+  const data = {
+    name: [
+      { data:"somedata", num: 13000 },
+      {data:"somedata", num: 16500 },
+      { data:"somedata", num: 14250 },
+      { data:"somedata", num: 19000 }
+    ],
+    id: [1, 2, 3, 4]
+  };
+  return (
+    <div className={classes.root}>
+      {data.id.map((elem) => (
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          justify="flex-start"
+          alignItems="flex-start"
+        >
+          {data.name.map((elem) => (
+            <Grid item xs={3} key={data.name.indexOf(elem)}>
+              <Card>
+                <CardHeader
+                  title={`quarter : ${elem.data}`}
+                  subheader={`earnings : ${elem.num}`}
+                />
+                <CardContent>
+                  <Typography variant="h5" gutterBottom>
+                  content
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      ))}
+    </div>
+  );
+}
+export default CardGrid;
